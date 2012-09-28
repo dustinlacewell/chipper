d529451be8d5a28ad986fabddbe4a584b83ac939
Dustin Lacewell <dlacewell@gmail.com>
2012-09-28 12:38:57 -0500

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='chipper',
+    version="0.1",
+    py_modules=['chipper'],
+    provides=['chipper'],
+    requires=['deconf'],
+    author="Dustin Lacewell",
+    author_email="dlacewell@gmail.com",
+    url="https://github.com/dustinlacewell/chipper",
+    description="Refreshingly simple declarative logging that utilizes arbitrary tag sinks instead of traditional level handling"
+    long_description=open('README.md').read(),
+)
